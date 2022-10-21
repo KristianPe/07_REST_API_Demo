ea2fafb83c7505cda613feb711c6c3d24383d9be
Kristian Petrakovic <kristiankroatien@gmail.com>
2022-10-21 10:42:26 +0000

    get200

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ app.listen(
         PORT,
         () => console.log(`Server ready at http://localhost:${PORT}`)
 );
+
+// Endpoint GET
+
+app.get('/store',(req,res) =>{
+    res.status(200).send({
+        product: "T-Shirt",
+        size: "L",
+        prize: 10.00,
+        hasLogo: false
+    })
+
+});
